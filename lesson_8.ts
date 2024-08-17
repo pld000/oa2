c9206739196225732ac7320b455c0e1a7b7e4b7b
vadim <pldsrt@gmail.com>
2024-08-17 17:32:43 +0300

    lesson 8

---
+++ lesson_8.ts
@@ -0,0 +1,25 @@
+class List<T> extends Array<T> {
+}
+
+class Device {
+}
+
+class InputDevice extends Device {
+}
+
+// Ковариантность
+// Список устройств, будет частным случаем списка устройств ввода
+const devicesList = new List<Device>();
+const inputDevicesList = new List<InputDevice>();
+
+// Контрвариантность
+// Метод plugIn DeviceManager<Device> сможет принять объект типа DeviceManager<InputDevice>, но не наоборот
+class DeviceManager<T> {
+    public plugIn(device: T): void {
+        // ...
+    }
+}
+
+const devicesManager = new DeviceManager<Device>();
+const inputDevicesManager = new DeviceManager<InputDevice>();
+
