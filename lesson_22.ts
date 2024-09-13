4c42eb3614dbb30d5be25e56b65bf4dfbf6a6e3d
pld000 <pld000@yandex.ru>
2024-09-13 15:51:43 +0300

    - lesson 22

---
+++ lesson_22.ts
@@ -0,0 +1,36 @@
+class Processor {
+
+}
+
+class VideoCard {
+
+}
+
+class Ram {
+}
+
+class Motherboard {
+  public processor: Processor;
+  public videoCard: VideoCard;
+  public ram: Ram;
+
+  constructor(processor: Processor, videoCard: VideoCard, ram: Ram) {
+    this.processor = processor;
+    this.videoCard = videoCard;
+    this.ram = ram;
+  }
+}
+
+class AmdProcessor extends Processor {}
+class AbcVideoCard extends VideoCard {}
+class XyzRam extends Ram {}
+
+class AtxMotherboard extends Motherboard {
+  constructor() {
+    super(AmdProcessor, AbcVideoCard, XyzRam);
+  }
+}
+/**
+ * Различные устройства могут быть вставлены в материнскую плату, могут быть различные комбинации этих устройств.
+ * К тому же, родительские сущности разных устройств, автономны и каждый играет свою роль.
+ * */
